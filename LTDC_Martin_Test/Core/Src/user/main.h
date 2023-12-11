bc07c8b4a240c325c91aa5212d5937ef98a1bd40
Henry-Loi <91612066+Henry-Loi@users.noreply.github.com>
2023-12-11 23:16:43 +0800

    delete other projects

--- LTDC_Martin_Test/Core/Src/user/main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/* ********************
-	Some helper macros
-   ******************** */
-#define __forceinline __attribute__((__always_inline__)) inline
-
-#define get_ticks() HAL_GetTick()
